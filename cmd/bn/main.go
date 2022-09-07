9510f99be0586e351ea3da6aaf6f0323c50ea88b
Liopin <contact.pinheiro.lionel@gmail.com>
2022-09-07 13:58:35 +0000

    Get the current date in format it

--- cmd/bn/main.go
+++ cmd/bn/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+)
 
 func main() {
 	fmt.Println("Building my note command line tool")
+	brain := filepath.Dir("~/projects/test/dump/")
+	fmt.Println(brain + "/cap/")
+
+	current_time := time.Now()
+	var formatedDate string
+	formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d\n",
+		current_time.Year(), current_time.Month(), current_time.Day(),
+		current_time.Hour(), current_time.Minute(), current_time.Second())
+	fmt.Println(formatedDate)
 }
