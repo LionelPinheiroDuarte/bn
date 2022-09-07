6c9e2c29c18405a4ab9a85898df25b0ef551e2bc
Liopin <contact.pinheiro.lionel@gmail.com>
2022-09-07 14:21:35 +0000

    Clean date format and create directories

--- cmd/bn/main.go
+++ cmd/bn/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"time"
 )
 
 func main() {
 	fmt.Println("Building my note command line tool")
-	brain := filepath.Dir("~/projects/test/dump/")
+	brain := filepath.Dir("/home/boost/projects/test/dump/")
 	fmt.Println(brain + "/cap/")
 
 	current_time := time.Now()
 	var formatedDate string
-	formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d\n",
+	formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d",
 		current_time.Year(), current_time.Month(), current_time.Day(),
 		current_time.Hour(), current_time.Minute(), current_time.Second())
 	fmt.Println(formatedDate)
+
+	os.Chdir(brain)
+	os.Mkdir(formatedDate, 0750)
+
 }
