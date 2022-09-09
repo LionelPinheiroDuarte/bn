f1313606aa7feb3f25a9ecfa1b0c88a5270ef81b
Liopin <contact.pinheiro.lionel@gmail.com>
2022-09-09 18:17:46 +0000

    Clean code and dealing with dynamic dates

--- cmd/bn/main.go
+++ cmd/bn/main.go
@@ -3,23 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"reflect"
 	"time"
 )
 
 func main() {
-	fmt.Println("Building my note command line tool")
+
 	brain := filepath.Dir("/home/boost/projects/test/dump/")
-	fmt.Println(brain + "/cap/")
 
 	current_time := time.Now()
-	var formatedDate string
-	formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d",
+	var formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d",
 		current_time.Year(), current_time.Month(), current_time.Day(),
 		current_time.Hour(), current_time.Minute(), current_time.Second())
-	fmt.Println(formatedDate)
 
+	fmt.Println(reflect.TypeOf(formatedDate))
 	os.Chdir(brain)
 	os.Mkdir(formatedDate, 0750)
+	fmt.Println(formatedDate + "25")
+	os.Chdir(formatedDate)
+	fmt.Println(formatedDate + "27")
+	_, err := os.Create("README.md")
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	cmd := exec.Command("vi", "README.md")
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
