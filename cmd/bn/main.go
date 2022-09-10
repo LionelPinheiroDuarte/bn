d3e400dc5b2df44737ca2ec180a7136390f42960
Liopin <contact.pinheiro.lionel@gmail.com>
2022-09-10 15:40:11 +0000

    Finish core functionalities

--- cmd/bn/main.go
+++ cmd/bn/main.go
@@ -2,35 +2,61 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"reflect"
 	"time"
 )
 
-func main() {
-
-	brain := filepath.Dir("/home/boost/projects/test/dump/")
-
+func formatedDate() string {
 	current_time := time.Now()
 	var formatedDate = fmt.Sprintf("%d%02d%02d%02d%02d%02d",
 		current_time.Year(), current_time.Month(), current_time.Day(),
 		current_time.Hour(), current_time.Minute(), current_time.Second())
+	return formatedDate
+}
+func createDirectory(title string) {
+	os.Mkdir(title, 0750)
+	fmt.Println(title)
+	os.Chdir(title)
+	fmt.Println(title)
+}
+func main() {
 
-	fmt.Println(reflect.TypeOf(formatedDate))
+	brain := filepath.Dir("/home/boost/projects/test/dump/")
+
+	formatedDate := formatedDate()
+
 	os.Chdir(brain)
-	os.Mkdir(formatedDate, 0750)
-	fmt.Println(formatedDate + "25")
-	os.Chdir(formatedDate)
-	fmt.Println(formatedDate + "27")
-	_, err := os.Create("README.md")
+	createDirectory(formatedDate)
+
+	path, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(path)
 
-	cmd := exec.Command("vi", "README.md")
-	if err := cmd.Run(); err != nil {
+	file, err := os.Create("README.md")
+	if err != nil {
 		fmt.Println(err)
+	}
+	fmt.Println(file)
+
+	lscmd := exec.Command("ls")
+	output, err := lscmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Output \n%s\n", string(output))
+
+	fmt.Println("exe vi")
+	cmd := exec.Command("vim", "README.md")
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	oops := cmd.Run()
+	if err != nil {
+		log.Fatal(oops)
 	}
 }
